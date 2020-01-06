aab5c49ede95d803892278621d6eb1311d6ea082
Untanky <mail@lukasgrimm.com>
2020-01-06 13:48:36 +0100

    Implemented SkeletonLoader

---
+++ SkeletonLoader.cpp
@@ -0,0 +1,63 @@
+#include "SkeletonLoader.h"
+
+#include "glm/gtc/type_ptr.hpp"
+
+SkeletonLoader::SkeletonLoader(xml_node<>* node, vector<string> boneOrder)
+	: armatureData(node), boneOrder(boneOrder)
+{ }
+
+SkeletonData SkeletonLoader::extractSkeletonData()
+{
+	xml_node<>* headNode = armatureData->first_node("node\0");
+	JointData headJoint = loadJointData(headNode, true);
+	return SkeletonData(jointCount, headJoint);
+}
+
+JointData SkeletonLoader::loadJointData(xml_node<>* jointNode, bool isRoot)
+{
+	JointData joint = extractMainData(jointNode, isRoot);
+
+	xml_node<>* node = jointNode->first_node("node\0");
+	while (node)
+	{
+		joint.addChild(loadJointData(node, false));
+		node->next_sibling("node\0");
+	}
+	
+	return joint;
+}
+
+JointData SkeletonLoader::extractMainData(xml_node<>* jointNode, bool isRoot)
+{
+	string nameId = jointNode->first_attribute("id\0")->value();
+	int index = find(nameId.begin(), nameId.end(), nameId) - nameId.begin();
+	string rawData;
+	vector<string> matrixData;
+	string delimiter = " ";
+
+	size_t pos = 0;
+	std::string token;
+	while ((pos = rawData.find(delimiter)) != std::string::npos) {
+		token = rawData.substr(0, pos);
+		matrixData.push_back(token);
+		rawData.erase(0, pos + delimiter.length());
+	}
+
+	glm::mat4 matrix = glm::make_mat4(convertData(matrixData));
+	matrix = glm::transpose(matrix);
+
+	jointCount++;
+	return JointData(index, nameId, matrix);
+}
+
+float* SkeletonLoader::convertData(vector<string> rawData)
+{
+	float matrixData[16];
+
+	for (int i = 0; i < 16; i++)
+	{
+		matrixData[i] = stof(rawData[i]);
+	}
+
+	return matrixData;
+}
--- SkeletonLoader.h
+++ SkeletonLoader.h
@@ -2,13 +2,34 @@
 
 #include "rapidxml-1.13/rapidxml.hpp"
 #include "SkeletonData.h"
+#include <string>
 
+#include "glm/gtc/matrix_transform.hpp"
+
+using namespace std;
 using namespace rapidxml;
+
+constexpr int BUFFER_SIZE = 16;
 
 class SkeletonLoader
 {
 public:
-	SkeletonLoader(xml_node<>* node, unsigned int maxWeights);
+	SkeletonLoader(xml_node<>* node, vector<string> boneOrder);
 
 	SkeletonData extractSkeletonData();
+
+private:
+	JointData loadJointData(xml_node<>* jointNode, bool isRoot);
+
+	JointData extractMainData(xml_node<>* jointNode, bool isRoot);
+
+	float* convertData(vector<string>);
+
+	xml_node<>* armatureData;
+
+	vector<string> boneOrder;
+
+	int jointCount = 0;
+
+	const glm::mat4 CORRECTION = glm::rotate(glm::mat4(1), glm::pi<float>() / 360.f * 90.f, glm::vec3(1.f, 0.f, 0.f));
 };
